e365cda1387b544e17fe374dd573ade0cb5de177
Brian Colantonio <BrianJC87@gmail.com>
2018-05-22 16:46:24 -0400

    curl produces good array

--- curl.php
+++ curl.php
@@ -5,23 +5,28 @@
 		
 		//set URL and other appropriate options
 		curl_setopt($curl, CURLOPT_URL, $url);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		
 		
 		//grab URL and pass it to the browser
-		$data = curl_exec($curl);
+		$result = curl_exec($curl);
 		
 		//array_unique($links);
 		
 		//match page links
-		preg_match_all("/(https?:\/\/[\da-z\.-]+\.[a-z\.]{2,6}[\/\w \.-]+)/",$data, $match);
+		preg_match_all("/(https?:\/\/[\da-z\.-]+\.[a-z\.]{2,6}[\/\w \.-]+)/",$result, $match);
+		
+		$links = array_unique($match[0]);
+		
+		print_r($links);
 				
-		//parse_str($match, $links);
+		/* //parse_str($match, $links);
 		$match = array_map("unserialize", array_unique(array_map("serialize", $match)));
 		
 		var_dump($match);
-		//var_dump($data);
-		var_dump($links);
+		//var_dump($result);
+		var_dump($links[0]);
 		
 		//var_dump(array_unique($match));
 			
@@ -29,7 +34,7 @@
 		/* $links[1] = $match[1];
 		echo ($links[1]); */
 		
-		//close cURL resource, and free up system resources
+		//close cURL resource, and free up system resources */
 		curl_close($curl);
 	
 ?>
